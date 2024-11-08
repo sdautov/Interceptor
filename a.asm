44f98bf975dcec9ce52c7fc0db3898aa05ae0a72
sdautov <162040999+sdautov@users.noreply.github.com>
2024-11-08 10:51:59 +0500

    Create a.asm

---
+++ a.asm
@@ -0,0 +1,6 @@
+local_24[iVar3] = *(char *)(iVar1 + param_1);
+
+MOV        DL,byte ptr [ECX + EBX*0x1]
+MOV        byte ptr [ESP + EAX*0x1 + local_24],DL
+
+local_24[iVar3] = param_1[piVar5[iVar1]];
